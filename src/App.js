b101dfcef47c8ddfc2823b8544dd940ff0ac5855
JessieYan214 <jessieyan214@gmail.com>
2022-12-06 11:32:49 +1300

    delete info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import MainScreen from './MainScreen/index';
 import ResizeViewer from './ResizeViewer';
-// import yuzhi from './yuzhi';
-// import Radiomics from './Radiomics';
 
 function App() {
   return (
     <div className="App" >
-      {/* <MainScreen/> */}
-      {/* <Radiomics /> */}
       <ResizeViewer/>
-      {/* <yuzhi/> */}
     </div>
   )
 }
